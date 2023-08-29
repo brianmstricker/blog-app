bf06405587b391bc268f91ad92a2d5cc3491bd40
brianmstricker <brianmstricker@gmail.com>
2023-08-29 19:26:35 -0400

    updated cors

--- server/index.js
+++ server/index.js
@@ -14,7 +14,15 @@ import morgan from "morgan";
 const app = express();
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://blog-app-frontend-kz1l.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const connect = async () => {
