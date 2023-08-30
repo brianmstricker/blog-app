5a27916943860ac67e772602a71ed360888223ca
brianmstricker <brianmstricker@gmail.com>
2023-08-29 23:30:16 -0400

    test with origin true

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ import morgan from "morgan";
 const app = express();
 app.use(morgan("dev"));
 app.use(cookieParser());
+app.use(cors({ origin: true, credentials: true }));
 // app.use(
 //   cors({
 //     credentials: true,
